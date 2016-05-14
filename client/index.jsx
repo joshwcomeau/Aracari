83538824d4730c72e0450ac98bc717f1f35ba5c4
Joshua Comeau <joshwcomeau@gmail.com>
2016-05-14 17:42:21 -0400

    Remove smooch :(
    Sadly, as great as Smooch is, it doesn't look like I really need it
    for this project.

--- client/index.jsx
+++ client/index.jsx
@@ -8,7 +8,6 @@ import configureStore from 'store';
 import Header from 'components/Header';
 import Budget from 'components/Budget';
 import Home from 'components/Home';
-import SmoochWidget from 'components/SmoochWidget';
 
 import 'scss/main.scss';
 
@@ -21,7 +20,6 @@ render((
     <Home>
       <Header />
       <Budget />
-      <SmoochWidget />
     </Home>
   </Provider>
 ), document.getElementById('render-target'));
