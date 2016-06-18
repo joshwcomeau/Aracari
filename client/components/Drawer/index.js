2a63e293dd22b0275f7bd3db6430ef7d4ed022b1
Joshua Comeau <joshwcomeau@gmail.com>
2016-06-18 07:34:41 -0400

    Use lower-level animations to control drawer
    Using ReactTransitionGroup to manually specify the drawer open/close
    animation. This way I can dynamically set the amount of translating.
    
    These drawers have been a huge pain, but I'm close to building
    something truly worthwhile :)

--- client/components/Drawer/index.js
+++ client/components/Drawer/index.js
@@ -1,47 +1,96 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import classNames from 'classnames';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import ReactTransitionGroup from 'react-addons-transition-group';
 
 import 'scss/drawer.scss';
 
 
 // eslint-disable-next-line react/prop-types
-const DrawerContents = ({ children, title, className, onClose }) => {
-  const classes = classNames(['drawer', className]);
+class Drawer extends Component {
+  componentWillEnter(callback) {
+    // On enter, we need to figure out the drawer's height, translate it down
+    // by that amount, and then animate it back up.
+    const currentHeight = this.getDrawerHeight();
+    this.contentElem.style.transform = `translateY(${currentHeight}px)`;
 
-  return (
-    <div className={classes}>
-      <div
-        className="backdrop"
-        onClick={onClose}
-      />
-      <div className="content">
-        <header className="drawer-header">
-          {title}
-          <button onClick={onClose}>
-            <i className="material-icons">close</i>
-          </button>
-        </header>
-        {children}
-        <div className="drawer-footer-spacer" />
+    // Move it up 10px at a time until it's in the right place
+    this.slideDrawer({
+      start: currentHeight,
+      end: 0,
+      direction: 'up',
+      callback,
+    });
+  }
+
+  componentWillLeave(callback) {
+    const currentHeight = this.getDrawerHeight();
+
+    this.slideDrawer({
+      start: 0,
+      end: currentHeight,
+      direction: 'down',
+      callback,
+    });
+  }
+
+  slideDrawer({ start, end, direction = 'up', callback }) {
+    window.requestAnimationFrame(() => {
+      const finished = direction === 'up' ? start <= end : start >= end;
+
+      if (finished) { return callback(); }
+
+      const newOffset = direction === 'up' ? start - 10 : start + 10;
+      this.contentElem.style.transform = `translateY(${newOffset}px)`;
+
+      return this.slideDrawer({
+        start: newOffset,
+        end,
+        direction,
+        callback,
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.drawerHeight = this.getDrawerHeight();
+  }
+  componentDidUpdate() {
+    this.drawerHeight = this.getDrawerHeight();
+  }
+
+  getDrawerHeight() {
+    return this.contentElem && this.contentElem.offsetHeight;
+  }
+
+  render() {
+    const { title, children, className, onClose } = this.props;
+    const classes = classNames(['drawer', className]);
+
+    return (
+      <div className={classes}>
+        <div
+          className="backdrop"
+          onClick={this.props.onClose}
+        />
+        <div className="content" ref={el => { this.contentElem = el; }}>
+          <header className="drawer-header">
+            {title}
+            <button onClick={onClose}>
+              <i className="material-icons">close</i>
+            </button>
+          </header>
+          {children}
+          <div className="drawer-footer-spacer" />
+        </div>
       </div>
-    </div>
-  );
-};
-
-const Drawer = ({ isOpen, ...props }) => (
-  <ReactCSSTransitionGroup
-    transitionName="drawer"
-    transitionEnterTimeout={450}
-    transitionLeaveTimeout={600}
-  >
-    {isOpen ? <DrawerContents {...props} /> : null}
-  </ReactCSSTransitionGroup>
-);
+    );
+  }
+}
 
 
 Drawer.propTypes = {
   children: PropTypes.node,
+  title: PropTypes.string,
   className: PropTypes.string,
   onClose: PropTypes.func,
   isOpen: PropTypes.bool,
@@ -53,4 +102,10 @@ Drawer.propTypes = {
 
 Drawer.defaultProps = {};
 
-export default Drawer;
+const DrawerWrapper = props => (
+  <ReactTransitionGroup>
+    {props.isOpen ? <Drawer {...props} /> : null}
+  </ReactTransitionGroup>
+);
+
+export default DrawerWrapper;
