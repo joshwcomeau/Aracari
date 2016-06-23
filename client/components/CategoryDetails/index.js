5c02f5eaa204614b6de240e75757fa03241ad44b
Joshua Comeau <joshwcomeau@gmail.com>
2016-06-23 07:11:36 -0400

    Style CategoryDetails page, add summary card

--- client/components/CategoryDetails/index.js
+++ client/components/CategoryDetails/index.js
@@ -3,24 +3,24 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 import Avatar from 'material-ui/Avatar';
+import IconButton from 'material-ui/IconButton';
 import {
   Card, CardActions, CardHeader, CardMedia, CardTitle, CardText,
 } from 'material-ui/Card';
 import CategoryChart from 'components/CategoryChart';
 import CategoryItem from 'components/CategoryItem';
 
+import { formatCurrency } from 'utils/currency.utils';
 import 'scss/category-details.scss';
 
 
 const CategoryDetails = ({ category }) => {
   return (
     <div id="category-details">
-      <Card>
+      <Card style={{ marginBottom: '16px' }}>
         <CardHeader
           title={category.label}
-          subtitle="Monthly Expenses"
-          actAsExpander
-          showExpandableButton
+          subtitle="Category Details"
           style={{ marginTop: '3px', fontWeight: 'bold' }}
           subtitleStyle={{ marginTop: '6px', fontWeight: 'normal' }}
           avatar={
@@ -30,11 +30,32 @@ const CategoryDetails = ({ category }) => {
               backgroundColor={category.colour}
             />
           }
-        />
+        >
+          <IconButton className="category-edit-button">
+            <i className="material-icons">settings</i>
+          </IconButton>
+        </CardHeader>
         <CardText>
-          <CategoryChart category={category} />
+          <div className="flex-row centered with-border category-summary">
+            <div className="one-half">
+              <h3>Monthly Budget</h3>
+              <h5>{formatCurrency(category.limit)}</h5>
+            </div>
+            <div className="one-half">
+              <h3>Amount Remaining</h3>
+              <h5>{formatCurrency(category.limit)}</h5>
+            </div>
+          </div>
         </CardText>
+      </Card>
 
+      <Card>
+        <CardHeader
+          title="Monthly Expenses"
+          actAsExpander
+          showExpandableButton
+          style={{ fontWeight: 'bold' }}
+        />
         <CardText>
           {
             category.items.map(item => (
@@ -55,6 +76,7 @@ CategoryDetails.propTypes = {
   category: PropTypes.shape({
     slug: PropTypes.string,
     label: PropTypes.string,
+    limit: PropTypes.number,
     icon: PropTypes.string,
     colour: PropTypes.string,
     items: PropTypes.array,
